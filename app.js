0039b785b71bc50d65196ec53023682f96ecf481
WillyRumapea <willyrumapea123jr@gmail.com>
2025-07-30 01:16:54 +0700

    belajar deploy api

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const data = require("./data.json");
+
+app.get("/", (req, res) => {
+  res.send("API Ready!");
+});
+
+app.get("/list-name", (req, res) => {
+  res.json(data);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on: ${port}`);
+});
