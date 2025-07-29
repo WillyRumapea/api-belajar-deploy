412164b27c534228d0957b717f5d9f68c4efe9ba
WillyRumapea <willyrumapea123jr@gmail.com>
2025-07-30 02:11:25 +0700

    debug port

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.get("/list-name", (req, res) => {
   res.json(data);
 });
 
+console.log("PORT dari ENV:", process.env.PORT);
+
 app.listen(PORT, () => {
   console.log(`Server running on: ${PORT}`);
 });
