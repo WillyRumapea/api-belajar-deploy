151eaf698085ae25aa0a50825d7c840a2052c1c2
WillyRumapea <willyrumapea123jr@gmail.com>
2025-07-30 12:58:21 +0700

    connect ke db

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const data = require("./data.json");
+const pool = require("./controller/conn");
 
 app.get("/", (req, res) => {
   res.send("API Ready!");
 });
 
-app.get("/list-name", (req, res) => {
-  res.json(data);
+app.get("/list-name", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM belajar.product_table");
+    res.json(result.rows);
+  } catch (err) {
+    console.log("Database query error:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 console.log("PORT dari ENV:", process.env.PORT);
